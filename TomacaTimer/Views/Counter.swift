22df0ebb1a733f899bb02a5b27e7c979e9f9cf9e
diana.hernandez <diana.hernandez@xing.com>
2022-04-01 22:26:55 +0200

    Add minutes and seconds to the counter ⏱

--- TomacaTimer/Views/Counter.swift
+++ TomacaTimer/Views/Counter.swift
@@ -3,27 +3,30 @@ import SwiftUI
 struct Counter: View {
     var viewModel: TimerViewModel
     var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    @State var timeRemaining: Int
+    @State var minutes: Int
+    @State var seconds: Int = 60
     var goTo: () -> Void
-    
+
     var body: some View {
-        Text("\(timeRemaining)")
-            .fontWeight(.semibold)
-            .font(.title)
-            .onReceive(self.timer) { _ in
-                if self.timeRemaining > 0 {
-                    self.timeRemaining -= 1
-                }
-                else {
-                    goTo()
-                }
-            }.frame(maxWidth: .infinity)
-            .padding()
+        VStack {
+            Text("\(minutes):\(seconds)")
+                .fontWeight(.semibold)
+                .font(.title)
+                .onReceive(self.timer) { _ in
+                    if self.minutes > 0 {
+                        self.minutes -= 1
+                    }
+                    else {
+                        goTo()
+                    }
+                }.frame(maxWidth: .infinity)
+                .padding()
+            }
         }
     }
 
 struct Counter_Previews: PreviewProvider {
     static var previews: some View {
-        Counter(viewModel: TimerViewModel(), timeRemaining: 30, goTo: { print("") })
+        Counter(viewModel: TimerViewModel(), minutes: 30, goTo: { print("") })
     }
 }
