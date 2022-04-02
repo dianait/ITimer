27acf56f0b8f257f50fa6bdf1e8421b75e9c759b
diana.hernandez <diana.hernandez@xing.com>
2022-04-03 00:55:54 +0200

    Add getLongBreak at StorageController ⏳

--- TomacaTimer/StorageController.swift
+++ TomacaTimer/StorageController.swift
@@ -14,9 +14,36 @@ struct WorkString: Hashable, Encodable, Decodable {
 
 class StorageController {
     var workList: [Work] = []
+    var userLongBrake: Int = 15
     
     init(){
         self.workList = get()
+        self.userLongBrake = getLongBrake()
+    }
+    
+    func saveLongBrake(userLongBrake: Int) {
+        do {
+            let encoder = JSONEncoder()
+            let data = try encoder.encode(userLongBrake)
+            UserDefaults.standard.set(data, forKey: "userLongBrake")
+            }
+        catch {
+            print("Unable to Encode Score Array (\(error))")
+        }
+    }
+    
+    func getLongBrake() -> Int {
+        var userLongBrake: Int = 15
+        if let data = UserDefaults.standard.data(forKey: "userLongBrake") {
+            do {
+                let decoder = JSONDecoder()
+                userLongBrake = try decoder.decode(Int.self, from: data)
+                return userLongBrake
+            } catch {
+                print("Unable to Decode Score Array (\(error))")
+            }
+        }
+        return userLongBrake
     }
     
     func save(workList: [Work]) {
