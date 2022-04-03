40407610c2eb9cefa1d93bb7c09b3e4b2bc41112
diana.hernandez <diana.hernandez@xing.com>
2022-04-03 14:11:25 +0200

    Add TimerController but it's not working yet 🤷‍♀️

--- TomacaTimer/Controllers/TimerController.swift
+++ TomacaTimer/Controllers/TimerController.swift
@@ -1,8 +1,48 @@
-//
-//  TimerController.swift
-//  TomacaTimer
-//
-//  Created by Diana Hernández on 3/4/22.
-//
-
 import Foundation
+import Combine
+import SwiftUI
+
+class TimerController {
+    @State var timer: Timer.TimerPublisher = Timer.publish(every: 1, on: .main, in: .common)
+    @State var connectedTimer: Cancellable? = nil
+    @State var isPaused: Bool
+    @State var timeRemaing: Int
+    
+    init(isPaused: Bool, timeRemaing: Int){
+        self.timer = Timer.publish(every: 1, on: .main, in: .common)
+        self.connectedTimer = nil
+        self.isPaused = isPaused
+        self.timeRemaing = timeRemaing
+    }
+        
+    private func instantiateTimer() {
+           self.isPaused = false
+           self.timer = Timer.publish(every: 1, on: .main, in: .common)
+           self.connectedTimer = self.timer.connect()
+           return
+       }
+       
+    private func cancelTimer() {
+           self.isPaused = true
+           self.connectedTimer?.cancel()
+           return
+       }
+       
+    private func resetCounter() {
+           self.timeRemaing = 0
+           return
+       }
+       
+    private func restartTimer() {
+           self.timeRemaing = 1 * 60
+           self.cancelTimer()
+           self.instantiateTimer()
+           return
+       }
+    
+    private func timeString(time: Int) -> String {
+           let minutes = Int(time) / 60 % 60
+           let seconds = Int(time) % 60
+           return String(format:"%02i:%02i", minutes, seconds)
+       }
+}
