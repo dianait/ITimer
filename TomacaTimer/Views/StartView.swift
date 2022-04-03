fdafd1a1153f76c4cfe81f011bbf95b6a4354bad
diana.hernandez <diana.hernandez@xing.com>
2022-04-03 13:04:03 +0200

    Change button text to Spanish

--- TomacaTimer/Views/StartView.swift
+++ TomacaTimer/Views/StartView.swift
@@ -1,39 +1,55 @@
 import SwiftUI
+import AlertToast
 
 struct StartView: View {
     var viewModel: TimerViewModel
     @State private var task: String = ""
+    @State private var showToast = false
     
     var body: some View {
         VStack {
             Image("logo")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-            TextField("what you want to do?", text: self.$task)
+            TextField("Escribe tu tarea", text: self.$task)
             .padding()
             .shadow(color: Color.gray, radius: 15, x: 10, y: 10)
             .font(.custom("Open Sans", size: 18))
-            .frame(width: 250)
             .border(Color.gray)
+            .background(Color.white)
+            .padding()
             
-            Button("⏱ START"){
-                if task != "" {
-                    viewModel.setAndStart(task: task)
-                }
+            Button("⏱ EMPEZAR"){
+                if task != "" { viewModel.setAndStart(task: task) }
+                else { self.showToast = true }
             }.padding()
             .frame(width: 250)
             .background(Color(red: 0.9, green: 0.2, blue: 0.1))
             .foregroundColor(.white)
             .clipShape(Capsule())
             .shadow(color: Color.gray, radius: 1, x: 2, y: 2)
-            Button("⚙️ SETTINGS"){
-                viewModel.goToSettings()
+            Button("⚙️ AJUSTES"){
+                viewModel.settings()
             }.padding()
             .frame(width: 250)
             .background(Color(red: 0.4, green: 0.6, blue: 0.9))
             .foregroundColor(.white)
             .clipShape(Capsule())
             .shadow(color: Color.gray, radius: 1, x: 2, y: 2)
+            Button("⚙️ PROGRESO"){
+                viewModel.settings()
+            }.padding()
+            .frame(width: 250)
+            .background(Color(red: 0.4, green: 0.6, blue: 0.9))
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+            .shadow(color: Color.gray, radius: 1, x: 2, y: 2)
+
+        }.frame(maxHeight: .infinity)
+            .background(Color(red: 0.9, green: 1, blue: 1))
+            
+        .toast(isPresenting: $showToast, duration: 3){
+            AlertToast(displayMode: .hud, type: .regular, title: "⚠️ Necesitas escribir la tarea")
         }
        
     }
